0dd7868742073f625f1119dd3eae1ca35565c169
Olivia Davies <odavies@vcd-ceb131-18.eastern.ewu.edu>
2017-05-09 16:10:02 -0700

    worked on API
    *pulled data from DropMark

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,27 @@
 
 // Configure JSON Call
 
-// Get, Set 
 
-// Display it
+// The API feed
+var getStuff = 'https://oliviadavies.dropmark.com/420953.json';
+
+// Optional settings for private collections. This is public, no settings needed. 
+var setStuff = {};
+
+// Do stuff with returned data
+var doStuff = function(data) {
+
+  console.log(data.items);
+  console.log(data.items[0].link); 
+  console.log(data.items[0].name); 
+  console.log(data.items[0].thumbnails.cropped); 
+  
+  // What do you want to do? Show (display on page) data? 
+    //  showStuff(data);
+  
+};
+              
+              
+//GET JSON FROM API
 
 $.getJSON(getStuff, setStuff, doStuff);
